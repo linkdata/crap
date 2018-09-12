046393a66ad656ebf29a4fcbacfd9afae83fcc3d
Johan Lindh <johan@linkdata.se>
2018-09-12 12:54:51 +0200

    docs

--- rap_conn.hpp
+++ rap_conn.hpp
@@ -23,18 +23,28 @@ public:
         , frame_ptr_(frame_buf_)
     {
     }
+
     virtual ~conn() {}
 
+    /**
+     * @brief write() calls the @a conn_write_cb, writing @a n bytes at @a p to the network.
+     * 
+     * @param p the bytes to write, must not be NULL
+     * @param n the number of bytes to write
+     * @return int return value from #conn_write_cb
+     */
     int write(const char* p, int n) const
     {
         return conn_write_cb_(conn_user_data_, p, n);
     }
 
-    // consume up to 'len' bytes of data from 'p',
-    // returns the number of bytes consumed, or less if there is an error.
-
-    // consume up to 'len' bytes of data from 'p',
-    // returns the number of bytes consumed, or less if there is an error.
+    /**
+     * @brief consume up to @a src_len bytes of data from @a src_buf
+     * 
+     * @param src_buf the bytes to read, must not be NULL
+     * @param src_len number of bytes to read
+     * @return int number of bytes consumed, or less if there is an error.
+     */
     int recv(const char* src_buf, int src_len)
     {
         if (!src_buf || src_len < 0)
@@ -79,6 +89,12 @@ public:
         return (int)(src_ptr - src_buf);
     }
 
+    /**
+     * @brief Get the exchange object identified by it's ID
+     * 
+     * @param id the id number of the exchange
+     * @return rap::exchange* the exchange, or NULL if it was not found
+     */
     virtual rap::exchange* get_exchange(rap_exch_id id) = 0;
 
 protected:
